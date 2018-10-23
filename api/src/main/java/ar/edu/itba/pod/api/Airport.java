6c7899aece3155b72b0302ee40e92f5a3bf10029
Ariel <ariel.debrouvier@gmail.com>
2018-10-23 18:26:24 -0300

    Add csv reader and Airport pojo

---
+++ api/src/main/java/ar/edu/itba/pod/api/Airport.java
@@ -0,0 +1,50 @@
+package ar.edu.itba.pod.api;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+
+import java.io.IOException;
+
+public class Airport implements DataSerializable {
+
+    private String oaci;
+    private String iata;
+    private String name;
+    private String province;
+
+    public Airport(){}
+
+    public Airport(String oaci, String iata, String name, String province) {
+        this.oaci = oaci;
+        this.iata = iata;
+        this.name = name;
+        this.province = province;
+    }
+
+    @Override
+    public void writeData(ObjectDataOutput out) throws IOException {
+        out.writeUTF(oaci);
+        out.writeUTF(iata);
+        out.writeUTF(name);
+        out.writeUTF(province);
+    }
+
+    @Override
+    public void readData(ObjectDataInput in) throws IOException {
+        oaci = in.readUTF();
+        iata = in.readUTF();
+        name = in.readUTF();
+        province = in.readUTF();
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "oaci='" + oaci + '\'' +
+                ", iata='" + iata + '\'' +
+                ", name='" + name + '\'' +
+                ", province='" + province + '\'' +
+                '}';
+    }
+}
--- client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -1,15 +1,24 @@
 package ar.edu.itba.pod.client;
 
+import ar.edu.itba.pod.api.Airport;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Client {
 	private static Logger logger = LoggerFactory.getLogger(Client.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 		logger.info("tpe-hazelcast Client Starting ...");
 
 		ClientConfig clientConfig = new ClientConfig();
@@ -17,5 +26,30 @@ public class Client {
 
 
 		HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+
+		final IList<Object> airports = client.getList("airports");
+
+		List<String> airportLines = readFile("aeropuertos.csv", "utf-8");
+		List<String> movementLines = readFile("movimientos.csv", "iso-8859-1");
+
+		airportLines.stream().skip(1).map(line -> line.split(";"))
+				.map(val -> new Airport(val[1], val[2], val[4], val[21]))
+				.forEach(airports::add);
+	}
+
+	private static List<String> readFile(String file, String encoding) throws URISyntaxException {
+		logger.info("Inicio de la lectura del archivo");
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(
+					Paths.get(Client.class.getClassLoader().getResource(file).toURI()),
+					Charset.forName(encoding)
+			);
+		} catch (IOException | NullPointerException e) {
+			logger.error("Error de lectura del archivo");
+			System.exit(1);
+		}
+		logger.info("Fin de la lectura del archivo");
+		return lines;
 	}
 }
