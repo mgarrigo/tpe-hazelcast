a8aa21ce198c6ad6695ed6559c7ee3d92b717d5d
Mgarrigo <mariano.garrigo@gmail.com>
2018-11-01 00:23:34 -0300

    Fixed Query5.

--- api/src/main/java/ar/edu/itba/pod/api/reducers/Query5ReducerFactory.java
+++ api/src/main/java/ar/edu/itba/pod/api/reducers/Query5ReducerFactory.java
@@ -8,31 +8,33 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class Query5ReducerFactory implements ReducerFactory<Pair<String, String>, Boolean, Integer> {
 
-    @Override
-    public Reducer<Boolean, Integer> newReducer(Pair<String, String> pair) {
-        return new InternationalMovementsPercentageReducer();
-    }
+	@Override
+	public Reducer<Boolean, Integer> newReducer(Pair<String, String> pair) {
+		return new InternationalMovementsPercentageReducer();
+	}
 
-    private class InternationalMovementsPercentageReducer extends Reducer<Boolean, Integer> {
+	private class InternationalMovementsPercentageReducer extends Reducer<Boolean, Integer> {
 
-    	private AtomicLong internationalMovements;
-        private AtomicLong totalMovements;
+		private AtomicLong internationalMovements;
+		private AtomicLong totalMovements;
 
-        @Override
-        public void beginReduce () {
-            internationalMovements = new AtomicLong(0);
-            totalMovements = new AtomicLong(0);
-        }
+		@Override
+		public void beginReduce () {
+			internationalMovements = new AtomicLong(0);
+			totalMovements = new AtomicLong(0);
+		}
 
-        @Override
-        public void reduce(Boolean value) {
-            totalMovements.getAndAdd(1L);
-            if (value) internationalMovements.getAndAdd(1L);
-        }
+		@Override
+		public void reduce(Boolean value) {
+//			System.out.println(String.format("international = %s - total = %s", internationalMovements, totalMovements));
+			totalMovements.getAndAdd(1L);
+			if (value) internationalMovements.getAndAdd(1L);
+		}
 
-        @Override
-        public Integer finalizeReduce() {
-            return (int) Math.floor((double) internationalMovements.get() / (double) totalMovements.get());
-        }
-    }
+		@Override
+		public Integer finalizeReduce() {
+//			System.out.println(String.format("FinalizeReduce: %s", (int) Math.floor((double) internationalMovements.get() / (double) totalMovements.get())));
+			return (int) Math.floor(100.0 * (double) internationalMovements.get() / (double) totalMovements.get());
+		}
+	}
 }
